6a390ce62059f1c57e5a89b7f05e9750251274a6
Sujaul Miah <sujaul.m@gmail.com>
2019-11-22 14:51:14 +0000

    Created first test to get passenger name - getName test passes

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -3,4 +3,14 @@ public class Passenger {
     private String name;
     private int bags;
 
+    public Passenger(String name, int bags){
+        this.name = name;
+        this.bags = bags;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+
+    public void setup(){
+        passenger = new Passenger("Sujaul", 1);
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Sujaul", passenger.getName());
+    }
+
 }
