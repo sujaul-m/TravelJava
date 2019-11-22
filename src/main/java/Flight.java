d6ec887fd6577728403fe88aab85b690139a0533
Sujaul Miah <sujaul.m@gmail.com>
2019-11-22 16:16:15 +0000

    created tests for flight to get flight number, destination, departure time, available seats and book passengers - all tests pass

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,71 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private PlaneType plane;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(String flightNumber, PlaneType plane, String destination, String departureAirport, String departureTime){
+        this.passengers = new ArrayList<Passenger>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+
+    }
+
+    public String getFlightNo() {
+        return this.flightNumber;
+    }
+
+    public String getFlightDestination() {
+        return this.destination;
+    }
+
+    public String getDepartureAirport() {
+        return this.departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return this.departureTime;
+    }
+
+    public int getPassengersBookingSize() {
+        return this.passengers.size();
+    }
+
+    public boolean availableRoomOnFlight() {
+        return plane.getCapacity() >= passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger1) {
+        if (availableRoomOnFlight()) {
+            this.passengers.add(passenger1);
+        }
+    }
+
+    public int getAvailableSeats() {
+        return plane.getCapacity() - passengers.size();
+    }
+
+    public double getPlaneCapacity() {
+        return plane.getCapacity();
+    }
+
+    public double getPlaneWeight() {
+        return  plane.getWeight();
+    }
+
+    public double getAvailableLuggageWeight() {
+        return getPlaneWeight() / 5;
+    }
+
+    public int getNoOfPassengersBags(Passenger passenger) {
+        return passenger.getBags();
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,108 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Passenger passenger4;
+    Passenger passenger5;
+
+    @Before
+    public void setup() {
+        flight = new Flight("QT321", PlaneType.BOEING747, "IST", "EDI", "14:45");
+        passenger1 = new Passenger("Sujaul", 3);
+        passenger2 = new Passenger("Zsolt", 1);
+        passenger3 = new Passenger("Louis", 5);
+        passenger4 = new Passenger("Harrison", 2);
+        passenger5 = new Passenger("Hannah", 0);
+    }
+
+    @Test
+    public void flightHasANumberQT321(){
+        assertEquals("QT321", flight.getFlightNo());
+    }
+
+    @Test
+    public void  flightHasADestinationIstanbul(){
+        assertEquals("IST", flight.getFlightDestination());
+    }
+
+    @Test
+    public void flightHasADepartureEdinburgh(){
+        assertEquals("EDI", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void flightHasADepartureTime1100(){
+        assertEquals("14:45", flight.getDepartureTime());
+    }
+
+    @Test
+    public void getPlaneCapacity(){
+        assertEquals(10, flight.getPlaneCapacity(), 0.01);
+    }
+
+    @Test
+    public void getPlaneWeight(){
+        assertEquals(250.00, flight.getPlaneWeight(), 0.01);
+    }
+
+    @Test
+    public void getWeightAvailableForPassengerLuggage(){
+        assertEquals(50.00, flight.getAvailableLuggageWeight(), 0.01);
+    }
+
+    @Test
+    public void passengerNumbers(){
+        assertEquals(0, flight.getPassengersBookingSize());
+    }
+
+    @Test
+    public void addPassengerToFlightTrue(){
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger3);
+        assertEquals(3, flight.getPassengersBookingSize());
+    }
+
+    @Test
+    public void checkIfRoomOnFlightTrue(){
+        assertEquals(true, flight.availableRoomOnFlight());
+    }
+
+    @Test
+    public void checkIfRoomOnFlightFalse(){
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger3);
+        flight.addPassenger(passenger4);
+        flight.addPassenger(passenger4);
+        flight.addPassenger(passenger5);
+        flight.addPassenger(passenger5);
+        assertEquals(false, flight.availableRoomOnFlight());
+    }
+
+    @Test
+    public void checkAvailableSeats(){
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        flight.addPassenger(passenger3);
+        flight.addPassenger(passenger4);
+        assertEquals(6, flight.getAvailableSeats());
+    }
+
+    @Test
+    public void getNoOfPassengersBags(){
+        assertEquals(5, flight.getNoOfPassengersBags(passenger3));
+    }
+
 }
